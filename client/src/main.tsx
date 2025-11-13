6b9f63467826ec092149c2c29adb8f0e4278e7a2
omabo20 <72064792+omabo20@users.noreply.github.com>
2025-11-13 15:38:00 +0100

    Fix React app mounting

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,13 @@
-import { createRoot } from "react-dom/client";
+// client/src/main.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-const container = document.getElementById("root");
+const container = document.getElementById("root") as HTMLElement;
 
-if (container) {
-  createRoot(container).render(<App />);
-}
+ReactDOM.createRoot(container).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
